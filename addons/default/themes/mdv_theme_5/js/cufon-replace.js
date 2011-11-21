79151efe55d9ac8f51beba4d63976e1e34521af5
Joze Perez <jozeperezp@gmail.com>
2011-11-21 12:36:38 -0500

    Removed old default theme

--- addons/default/themes/mdv_theme_5/js/cufon-replace.js
+++
@@ -1,9 +0,0 @@
-// What to Replace??
-//Cufon.replace( 'h1, h2, h3', { fontFamily: 'sapient', hover:true });
-Cufon.replace('h1', { fontFamily: 'sapient', hover: true }); 
-Cufon.replace('h2', { fontFamily: 'sapient', hover: true }); 
-Cufon.replace('h3', { fontFamily: 'sapient', hover: true }); 
-Cufon.replace('h4', { fontFamily: 'sapient', hover: true });
-Cufon.replace('h5', { fontFamily: 'sapient', hover: true }); 
-Cufon.replace('h6', { fontFamily: 'sapient', hover: true });
-Cufon.replace('.header_small_box h2', { fontFamily: 'sapient', hover: true });
