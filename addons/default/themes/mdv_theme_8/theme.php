67c67885640f539970cac9ea399d08cebea59e25
Joze Perez <jozeperezp@gmail.com>
2011-11-21 12:32:50 -0500

    Updated Default Theme

---
+++ addons/default/themes/mdv_theme_8/theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MDV_theme_8 extends Theme {
+
+    public $name			= 'MDV Theme 8 - TonitoMazda.com';
+    public $author			= 'joze|Perez';
+    public $author_website	= 'http://jozeperez.com/';
+    public $website			= 'http://midealervirtual.com/';
+    public $description		= 'Dealer Theme 8 - TonitoMazda.com';
+    public $version			= '1.0';
+	public $options 		= array();
+}
+
+/* End of file theme.php */
